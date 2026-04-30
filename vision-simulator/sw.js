fb8fb262679b1aa2a2f9c0b8e21584c86c2141ff
ryan11evans <ryan11evans@me.com>
2026-04-30 09:53:18 -0700

    Update sw.js

--- vision-simulator/sw.js
+++ vision-simulator/sw.js
@@ -1,1 +1,11 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('vision-sim').then(cache => cache.addAll(['index.html']))
+  );
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
